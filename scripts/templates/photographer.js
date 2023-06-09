088020dbb49ed64a8fe4e90d0cbf296ffd3cbfaf
EJ <eric_j_fra@yahoo.fr>
2023-06-09 15:16:04 +0200

    dynamic style for home photographer section

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,34 +1,55 @@
 function photographerTemplate(data) {
     const { name, id, city, country, tagline, price, portrait } = data;
-    console.log(data);
+    // console.log(data);
 
-    const picture = `assets/images/photographers_id/${portrait}`;
+    //Retrieval PIcture of Photographer
+    let idForPicture = portrait
+    idForPicture = id
+    const picture = `assets/photographers/${id}.jpg`;
 
     function getUserCardDOM() {
         const article = document.createElement('article');
-        
-        const img = document.createElement('img');
-        img.setAttribute("src", picture)
-        img.setAttribute("alt", `${name} photographer portrait`)
-        
-        const h2 = document.createElement('h2');
-        h2.textContent = name;
 
+
+        //Photographer img Portrait
+        const imgContainer = document.createElement('div')
+        imgContainer.classList.add('img_container')
+        const imgForPhotographers = document.createElement('img');
+        imgForPhotographers.setAttribute("src", picture)
+        imgForPhotographers.setAttribute("alt", `${name} photographer portrait`)
+        imgForPhotographers.classList.add('photographer_img')
+
+        //Photographer Name
+        const photographerNameTitle = document.createElement('h2');
+        photographerNameTitle.textContent = name;
+        photographerNameTitle.classList.add('photographer_name')
+
+        //Photographer City
         const cityAndCountrySpan = document.createElement('span')
         cityAndCountrySpan.textContent = (`${city}, ${country}`)
+        cityAndCountrySpan.classList.add('photographer_city')
         
+        //Photographer Tagline
         const taglineSpan = document.createElement('span')
         taglineSpan.textContent = tagline
+        taglineSpan.classList.add('photographer_tagline')
 
+        // Photographer Price
         const pricePerDaySpan = document.createElement('span')
         pricePerDaySpan.textContent = (`${price}\u20AC/jour`)
+        pricePerDaySpan.classList.add('photographer_price')
 
+        article.appendChild(imgContainer)
+        imgContainer.appendChild(imgForPhotographers);
 
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(photographerNameTitle);
         article.appendChild(cityAndCountrySpan);
         article.appendChild(taglineSpan);
         article.appendChild(pricePerDaySpan)
+
+
+
+
 
         return (article);
     }
