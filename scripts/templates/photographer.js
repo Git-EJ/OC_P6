fa966d980981110bfc71830ba88e589486b7f20c
EJ <eric_j_fra@yahoo.fr>
2023-06-09 10:40:05 +0200

    fetch data photographers(homePage) && display without css

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,36 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
+    console.log(data);
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/images/photographers_id/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        img.setAttribute("alt", `${name} photographer portrait`)
+        
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+
+        const cityAndCountrySpan = document.createElement('span')
+        cityAndCountrySpan.textContent = (`${city}, ${country}`)
+        
+        const taglineSpan = document.createElement('span')
+        taglineSpan.textContent = tagline
+
+        const pricePerDaySpan = document.createElement('span')
+        pricePerDaySpan.textContent = (`${price}\u20AC/jour`)
+
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(cityAndCountrySpan);
+        article.appendChild(taglineSpan);
+        article.appendChild(pricePerDaySpan)
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, id, city, country, tagline, price, portrait, getUserCardDOM }
 }
