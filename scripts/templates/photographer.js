385f25c6bcaa5cc9132acaf1e66f23ae87e18728
EJ <eric_j_fra@yahoo.fr>
2023-06-13 12:19:19 +0200

    13 - Start index & photographer html accessibility - HTML and JS

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,22 +1,26 @@
 function photographerTemplate(data) {
-    const { name, id, city, country, tagline, price, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data
     // console.log(data);
     
     //Retrieval Picture of Photographer
     let idForPicture = portrait
     idForPicture = id
-    const picture = `assets/photographers/${id}.jpg`;
+    const picture = `assets/photographers/${id}.jpg`
     
     
     function getUserCardDOM() {
-        const article = document.createElement('article');
+        const article = document.createElement('article')
+        article.setAttribute("aria-labelledby", "Descriptions-des-photographes")
+        article.setAttribute("role", "group")
+        article.setAttribute("aria-label", "Description du photographe")
         
         //Photographer img Portrait
         const imgContainer = document.createElement('div')
         imgContainer.classList.add('img_container')
-        const imgForPhotographers = document.createElement('img');
+        const imgForPhotographers = document.createElement('img')
         imgForPhotographers.setAttribute("src", picture)
         imgForPhotographers.setAttribute("alt", `${name} photographer portrait`)
+        imgForPhotographers.setAttribute("aria-label", `photo portrait du photographe ${name}`)
         imgForPhotographers.classList.add('photographer_img')
         
         
@@ -27,24 +31,25 @@ function photographerTemplate(data) {
          * @param {String} addClass
          * @returns {HTMLElement} newly created 
          */
-        function addElement (tag, objectKey, addClass) {
+        function addElement (tag, objectKey, addClass, ariaLabel) {
             const newElement = document.createElement(tag)
             newElement.textContent = objectKey
             newElement.classList.add(addClass)
+            newElement.setAttribute("aria-label", ariaLabel)
             return newElement
         }
 
         // Photographer Name
-        const photographerName = addElement('h2', name, 'photographer_name')
+        const photographerName = addElement('h2', name, 'photographer_name', `nom du photographe: ${name}`)
         
         //Photographer City
-        const cityAndCountrySpan = addElement('span', (`${city}, ${country}`), 'photographer_city')
+        const cityAndCountrySpan = addElement('span', (`${city}, ${country}`), 'photographer_city', `ville et pays du photographe: ${city} ${country}`)
 
         //Photographer Tagline
-        const taglineSpan = addElement('span', tagline, 'photographer_tagline')
+        const taglineSpan = addElement('span', tagline, 'photographer_tagline', `devise du photographe: ${tagline}`)
         
         // Photographer Price
-        const pricePerDaySpan = addElement('span', (`${price}\u20AC/jour`), 'photographer_price')
+        const pricePerDaySpan = addElement('span', (`${price}\u20AC/jour`), 'photographer_price',`prix du photographe: ${price}\u20AC/jour`)
 
         
         article.appendChild(imgContainer)
